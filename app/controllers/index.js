163bccae56766cc264e124d5e04db7fc4f6c3f13
fgabriel78 <fgabriel.munoz@gmail.com>
2014-10-10 17:28:05 +0200

    Añadidos iconos a la botonera inferior

---
+++ app/controllers/index.js
@@ -0,0 +1,23 @@
+const number = 954433374;
+const url = "www.videoopen.es";
+
+function llamar() {
+	Ti.Platform.openURL('tel:+' + number );
+}
+
+function internet() {
+	Ti.Platform.openURL(url );
+}
+
+function correo() {
+	var emailDialog = Titanium.UI.createEmailDialog();
+ 
+	emailDialog.subject = "Contacta con VideoOpen";
+	emailDialog.toRecipients = ['info@videoopen.es'];
+	emailDialog.messageBody = "Solicitud de reserva";
+ 
+	emailDialog.open();
+}
+
+
+$.index.open();
